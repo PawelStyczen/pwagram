a185e41091b251f7b85be34cbd2f9a76fecbd27c
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2021-04-15 08:02:35 +0200

    adding notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,6 @@
 
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -22,3 +23,46 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function askForNotificationPermission() {
+  Notification.requestPermission((result)=>{
+    if(result !== 'granted') {
+      console.log('No notification permission granted!');
+    }else {
+      
+      displayConfirmNotification()
+
+      //TODO Hide button
+    }
+  })
+}
+
+function displayConfirmNotification() {
+  if('serviceWorker' in navigator) {
+
+    var options  = {
+      body: 'You Sucessfully subbed to our service',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      lang: 'en-US', //? BCP 47
+      vibrate: [100, 50, 200],
+      badge: '/src/images/icons/app-icons-96x96.png', //? only android
+      tag: 'confirm-notification', //? stacking notifications
+      renotify: true
+    };
+
+    //? Service worker registration allows for listening to sw events
+    navigator.serviceWorker.ready
+    .then((swreg) =>{
+      swreg.showNotification('Succesfully subscribed! (FROM Sw)',options);
+    })
+  }
+  
+}
+
+if ('Notification' in window){
+  enableNotificationsButtons.forEach((button) => {
+    button.style.display = 'inline-block';
+    button.addEventListener('click', askForNotificationPermission);
+  })
+}
--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts("/src/js/idb.js");
 importScripts("/src/js/utility.js");
 
-var CACHE_STATIC_NAME = "static-v31";
+var CACHE_STATIC_NAME = "static-v33";
 var CACHE_DYNAMIC_NAME = "dynamic-v4";
 var STATIC_FILES = [
   "/",
