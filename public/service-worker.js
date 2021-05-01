cf9251093e8b66a43d5fb9a1170d9edeaab92283
PawelStyczen <68872133+PawelStyczen@users.noreply.github.com>
2021-05-01 21:10:53 +0200

    workbox routing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,34 @@
-importScripts('workbox-sw.prod.v2.1.3.js');
+importScripts("workbox-sw.prod.v2.1.3.js");
 
-/**
- * DO NOT EDIT THE FILE MANIFEST ENTRY
- *
- * The method precache() does the following:
- * 1. Cache URLs in the manifest to a local cache.
- * 2. When a network request is made for any of these URLs the response
- *    will ALWAYS comes from the cache, NEVER the network.
- * 3. When the service worker changes ONLY assets with a revision change are
- *    updated, old cache entries are left as is.
- *
- * By changing the file manifest manually, your users may end up not receiving
- * new versions of files because the revision hasn't changed.
- *
- * Please use workbox-build or some other tool / approach to generate the file
- * manifest which accounts for changes to local files and update the revision
- * accordingly.
- */
-const fileManifest = [
+const workboxSW = new self.WorkboxSW();
+
+//* additional cache of all  google resources (fonts)
+workboxSW.router.registerRoute(
+  /.*(?:googleapis|gstatic)\.com.*$/,
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: "google-fonts",
+    cacheExpiration: {
+        maxEntries: 3,
+        maxAgeSeconds: 60 * 60 * 24 * 30
+    }
+  })
+);
+
+workboxSW.router.registerRoute(
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: "material-css",
+  })
+);
+
+workboxSW.router.registerRoute(
+  /.*(?:firebasestorage\.googleapis)\.com.*$/,
+  workboxSW.strategies.staleWhileRevalidate({
+    cacheName: "post-images",
+  })
+);
+
+workboxSW.precache([
   {
     "url": "404.html",
     "revision": "0a27a4163254fc8fce870c8cc3a3f94f"
@@ -25,10 +36,6 @@ const fileManifest = [
   {
     "url": "favicon.ico",
     "revision": "2cab47d9e04d664d93c8d91aec59e812"
-  },
-  {
-    "url": "help/index.html",
-    "revision": "ec3f1f60a56f0d8c9e62d61d1ea68afa"
   },
   {
     "url": "index.html",
@@ -43,6 +50,10 @@ const fileManifest = [
     "revision": "4a8def2516865c059c5e89b91bfbc296"
   },
   {
+    "url": "service-worker.js",
+    "revision": "f3cbce2e685721379cfff6b26e331eef"
+  },
+  {
     "url": "src/css/app.css",
     "revision": "f27b4d5a6a99f7b6ed6d06f6583b73fa"
   },
@@ -55,88 +66,8 @@ const fileManifest = [
     "revision": "1c6d81b27c9d423bece9869b07a7bd73"
   },
   {
-    "url": "src/images/icons/app-icon-144x144.png",
-    "revision": "83011e228238e66949f0aa0f28f128ef"
-  },
-  {
-    "url": "src/images/icons/app-icon-192x192.png",
-    "revision": "f927cb7f94b4104142dd6e65dcb600c1"
-  },
-  {
-    "url": "src/images/icons/app-icon-256x256.png",
-    "revision": "86c18ed2761e15cd082afb9a86f9093d"
-  },
-  {
-    "url": "src/images/icons/app-icon-384x384.png",
-    "revision": "fbb29bd136322381cc69165fd094ac41"
-  },
-  {
-    "url": "src/images/icons/app-icon-48x48.png",
-    "revision": "45eb5bd6e938c31cb371481b4719eb14"
-  },
-  {
-    "url": "src/images/icons/app-icon-512x512.png",
-    "revision": "d42d62ccce4170072b28e4ae03a8d8d6"
-  },
-  {
-    "url": "src/images/icons/app-icon-96x96.png",
-    "revision": "56420472b13ab9ea107f3b6046b0a824"
-  },
-  {
-    "url": "src/images/icons/apple-icon-114x114.png",
-    "revision": "74061872747d33e4e9f202bdefef8f03"
-  },
-  {
-    "url": "src/images/icons/apple-icon-120x120.png",
-    "revision": "abd1cfb1a51ebe8cddbb9ada65cde578"
-  },
-  {
-    "url": "src/images/icons/apple-icon-144x144.png",
-    "revision": "b4b4f7ced5a981dcd18cb2dc9c2b215a"
-  },
-  {
-    "url": "src/images/icons/apple-icon-152x152.png",
-    "revision": "841f96b69f9f74931d925afb3f64a9c2"
-  },
-  {
-    "url": "src/images/icons/apple-icon-180x180.png",
-    "revision": "2e5e6e6f2685236ab6b0c59b0faebab5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-57x57.png",
-    "revision": "cc93af251fd66d09b099e90bfc0427a8"
-  },
-  {
-    "url": "src/images/icons/apple-icon-60x60.png",
-    "revision": "18b745d372987b94d72febb4d7b3fd70"
-  },
-  {
-    "url": "src/images/icons/apple-icon-72x72.png",
-    "revision": "b650bbe358908a2b217a0087011266b5"
-  },
-  {
-    "url": "src/images/icons/apple-icon-76x76.png",
-    "revision": "bf10706510089815f7bacee1f438291c"
-  },
-  {
-    "url": "src/images/main-image-lg.jpg",
-    "revision": "31b19bffae4ea13ca0f2178ddb639403"
-  },
-  {
-    "url": "src/images/main-image-sm.jpg",
-    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
-  },
-  {
-    "url": "src/images/main-image.jpg",
-    "revision": "5c66d091b0dc200e8e89e56c589821fb"
-  },
-  {
-    "url": "src/images/sf-boat.jpg",
-    "revision": "0f282d64b0fb306daf12050e812d6a19"
-  },
-  {
     "url": "src/js/app.js",
-    "revision": "0e956cd7cd9f447536a1209659329cad"
+    "revision": "3f2b764c0cd46f27410aaf1bfb8043f5"
   },
   {
     "url": "src/js/feed.js",
@@ -163,10 +94,31 @@ const fileManifest = [
     "revision": "2778cd90d11750a8fa0b33a12ba0210e"
   },
   {
+    "url": "sw-base.js",
+    "revision": "22c3e1b7718e2cab5d08b95eba869fd3"
+  },
+  {
     "url": "sw.js",
     "revision": "a0155565c4d05506ed3df2d47b05aea3"
+  },
+  {
+    "url": "workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  },
+  {
+    "url": "src/images/main-image-lg.jpg",
+    "revision": "31b19bffae4ea13ca0f2178ddb639403"
+  },
+  {
+    "url": "src/images/main-image-sm.jpg",
+    "revision": "c6bb733c2f39c60e3c139f814d2d14bb"
+  },
+  {
+    "url": "src/images/main-image.jpg",
+    "revision": "5c66d091b0dc200e8e89e56c589821fb"
+  },
+  {
+    "url": "src/images/sf-boat.jpg",
+    "revision": "0f282d64b0fb306daf12050e812d6a19"
   }
-];
-
-const workboxSW = new self.WorkboxSW();
-workboxSW.precache(fileManifest);
+]);
